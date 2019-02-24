1936d08f98eb961cb8f470fe50d3bc5d11b72e33
Wojtek <wojtekj1989@gmail.com>
2019-02-24 16:31:09 +0100

    changing color theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+  iconfont: "md",
+  theme: {
+    primary: "#6785ff"
+  }
+});
