ae561e923798ca254f748a0adc78a60391c76d05
Wojtek <wojtekj1989@gmail.com>
2019-02-24 15:23:39 +0100

    created naked project structure

--- src/router.js
+++ src/router.js
@@ -1,23 +1,22 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./components/FlatbondForm.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "form",
+      component: () => import("./components/FlatbondForm.vue")
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/confirm",
+      name: "confirmation",
+      component: () => import("./components/FlatbondConfirmation.vue")
     }
   ]
-})
+});
